2ac395445a2565eebd149aa216ed26284430d68b
sirelon <sasha.sirelon@gmail.com>
2025-10-30 20:17:03 +0200

    fixed bundleId

--- composeApp/build.gradle.kts
+++ composeApp/build.gradle.kts
@@ -26,6 +26,7 @@ kotlin {
         iosTarget.binaries.framework {
             baseName = "ComposeApp"
             isStatic = true
+            binaryOptions["bundleId"] = "com.sirelon.aicalories"
         }
     }
     
