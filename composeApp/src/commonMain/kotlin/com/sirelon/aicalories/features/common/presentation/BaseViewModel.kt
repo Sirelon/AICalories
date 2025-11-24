1c5777d71adce3692ed7a0e15b95a80e7726a417
sirelon <sasha.sirelon@gmail.com>
2025-11-24 16:00:14 +0200

    Fix BaseViewModel lazy state init

--- composeApp/src/commonMain/kotlin/com/sirelon/aicalories/features/common/presentation/BaseViewModel.kt
+++ composeApp/src/commonMain/kotlin/com/sirelon/aicalories/features/common/presentation/BaseViewModel.kt
@@ -3,14 +3,18 @@ package com.sirelon.aicalories.features.common.presentation
 import androidx.lifecycle.ViewModel
 import kotlinx.coroutines.flow.MutableSharedFlow
 import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
 import kotlinx.coroutines.flow.asSharedFlow
-import kotlinx.coroutines.flow.asStateFlow
 import kotlinx.coroutines.flow.update
+import kotlin.LazyThreadSafetyMode
 
 abstract class BaseViewModel<State, Event, Effect> : ViewModel() {
 
-    private val _state = MutableStateFlow<State>(initialState())
-    val state = _state.asStateFlow()
+    private val _state: MutableStateFlow<State> by lazy(LazyThreadSafetyMode.NONE) {
+        MutableStateFlow(initialState())
+    }
+    val state: StateFlow<State>
+        get() = _state
 
     private val _effects = MutableSharedFlow<Effect>()
     val effects = _effects.asSharedFlow()
